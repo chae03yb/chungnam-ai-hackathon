d8d1b6d0dfc5068cd4e91a31f955feb30d10eb2c
gjrjsghl <aa01021107861@gmail.com>
2024-09-07 11:04:43 +0900

    divide screen

---
+++ lib/graphscene.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class graphPage extends StatelessWidget {
+  const graphPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
